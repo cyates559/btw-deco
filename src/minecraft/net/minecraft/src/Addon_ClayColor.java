d01a5ea9875d6b2cc369f0ebf556f4dec6a06190
Carsen Yates <fatburgeriii@gmail.com>
2017-07-06 18:30:22 -0700

    Bug fix for ClayColor not being loaded

--- src/minecraft/net/minecraft/src/Addon_ClayColor.java
+++ src/minecraft/net/minecraft/src/Addon_ClayColor.java
@@ -6,6 +6,8 @@ public class Addon_ClayColor
 		if(!AddonManager.isAddonLoaded("Clay")) return;
 		if(!AddonManager.isAddonLoaded("Flowers")) return;
 		for (int Index = 0; Index < 16; Index++)
+		{
 			FCRecipes.AddStokedCauldronRecipe(new ItemStack(Addon_Clay.stainedClay, 1, Index), new ItemStack[] { new ItemStack(Addon_Clay.hardenedClay), new ItemStack(Item.dyePowder, 1, Index+16) });
+		}
 	}
 }
