ddedb2bef786b9c09e580a6087322750b473cf1b
Unknown <jorgebonafe@gmail.com>
2018-04-07 12:11:29 -0300

    Fix bug when placing stained glass blocks on "replaceable" blocks like vines, snow, etc...

--- src/minecraft_server/net/minecraft/src/Addon_HayBale.java
+++ src/minecraft_server/net/minecraft/src/Addon_HayBale.java
@@ -1,16 +1,15 @@
 package net.minecraft.src;
-public class Addon_HayBale
-{
+
+public class Addon_HayBale {
 	public static Block hayBale;
-	public Addon_HayBale()
-	{
+
+	public Addon_HayBale() {
 		hayBale = new BlockBale(3025);
 		AddonManager.MakeStorage(Item.wheat, hayBale);
 	}
-	public static class BlockBale extends Block implements FCIBlock
-	{
-		public BlockBale(int id)
-		{
+
+	public static class BlockBale extends Block implements FCIBlock {
+		public BlockBale(int id) {
 			super(id, Material.cloth);
 			setUnlocalizedName("blockHay");
 			setStepSound(soundGrassFootstep);
@@ -19,87 +18,91 @@ public class Addon_HayBale
 			AddonManager.Register(this);
 			AddonManager.Name(this, "Hay Bale");
 		}
-		public boolean isOpaqueCube()
-		{
+
+		public boolean isOpaqueCube() {
 			return true;
 		}
-		public boolean renderAsNormalBlock()
-		{
+
+		public boolean renderAsNormalBlock() {
 			return true;
 		}
-		public boolean canDropFromExplosion(Explosion var1)
-		{
+
+		public boolean canDropFromExplosion(Explosion var1) {
 			return false;
 		}
-		public void onBlockDestroyedByExplosion(World world, int X, int Y, int Z, Explosion exp)
-		{
+
+		public void onBlockDestroyedByExplosion(World world, int X, int Y, int Z, Explosion exp) {
 			float v = 1.0F;
 
-			if (exp != null)
-			{
+			if (exp != null) {
 				v = 1.0F / exp.explosionSize;
 			}
 
-			for (int i=0;i<8;++i)
-				if (world.rand.nextFloat()<=v)
+			for (int i = 0; i < 8; ++i)
+				if (world.rand.nextFloat() <= v)
 					dropBlockAsItem_do(world, X, Y, Z, new ItemStack(Item.wheat));
 		}
-		
-		public int GetFacing(IBlockAccess access, int X, int Y, int Z)
-		{
-			return access.getBlockMetadata(X,Y,Z);
+
+		public int GetFacing(IBlockAccess access, int X, int Y, int Z) {
+			return access.getBlockMetadata(X, Y, Z);
 		}
-		public void SetFacing(World world, int X, int Y, int Z, int facing)
-		{
-			world.setBlockMetadataWithNotify(X,Y,Z,facing);
+
+		public void SetFacing(World world, int X, int Y, int Z, int facing) {
+			world.setBlockMetadataWithNotify(X, Y, Z, facing);
 		}
-		public int GetFacingFromMetadata(int meta)
-		{
+
+		public int GetFacingFromMetadata(int meta) {
 			return meta;
 		}
-		public int SetFacingInMetadata(int var1, int var2)
-		{
+
+		public int SetFacingInMetadata(int var1, int var2) {
 			return var2;
 		}
-		public boolean CanRotateOnTurntable(IBlockAccess access, int X, int Y, int Z)
-		{
-			return access.getBlockMetadata(X,Y,Z)!=0;
+
+		public boolean CanRotateOnTurntable(IBlockAccess access, int X, int Y, int Z) {
+			return access.getBlockMetadata(X, Y, Z) != 0;
 		}
-		public boolean CanTransmitRotationHorizontallyOnTurntable(IBlockAccess access, int X, int Y, int Z)
-		{
+
+		public boolean CanTransmitRotationHorizontallyOnTurntable(IBlockAccess access, int X, int Y, int Z) {
 			return true;
 		}
-		public boolean CanTransmitRotationVerticallyOnTurntable(IBlockAccess access, int X, int Y, int Z)
-		{
+
+		public boolean CanTransmitRotationVerticallyOnTurntable(IBlockAccess access, int X, int Y, int Z) {
 			return true;
 		}
-		public void RotateAroundJAxis(World world, int X, int Y, int Z, boolean var5)
-		{
+
+		public void RotateAroundJAxis(World world, int X, int Y, int Z, boolean var5) {
 			FCUtilsMisc.StandardRotateAroundJ(this, world, X, Y, Z, var5);
 		}
-		public int RotateMetadataAroundJAxis(int var1, boolean var2)
-		{
+
+		public int RotateMetadataAroundJAxis(int var1, boolean var2) {
 			return FCUtilsMisc.StandardRotateMetadataAroundJ(this, var1, var2);
 		}
-		public boolean ToggleFacing(World world, int X, int Y, int Z, boolean var5)
-		{
+
+		public boolean ToggleFacing(World world, int X, int Y, int Z, boolean var5) {
 			this.RotateAroundJAxis(world, X, Y, Z, var5);
 			return true;
 		}
-		public int onBlockPlaced(World var1, int var2, int var3, int var4, int var5, float var6, float var7, float var8, int var9)
-		{
-			if(var5<2)return 0;
-			else if(var5<4)return 1;
-			else return 2;
+
+		public int onBlockPlaced(World var1, int var2, int var3, int var4, int var5, float var6, float var7, float var8,
+				int var9) {
+			if (var5 < 2)
+				return 0;
+			else if (var5 < 4)
+				return 1;
+			else
+				return 2;
 		}
-		public void onBlockPlacedBy(World var1, int var2, int var3, int var4, EntityLiving var5, ItemStack var6)
-		{/*
-			int var7 = FCUtilsMisc.ConvertPlacingEntityOrientationToBlockFacing(var5);
-			
-			if(var7<2)var7=0;
-			else if(var7<4)var7=1;
-			else var7=2;
-			this.SetFacing(var1, var2, var3, var4, var7);//*/
+
+		public void onBlockPlacedBy(World var1, int var2, int var3, int var4, EntityLiving var5,
+				ItemStack var6) {/*
+													 * int var7 = FCUtilsMisc.
+													 * ConvertPlacingEntityOrientationToBlockFacing(var5);
+													 * 
+													 * if(var7<2)var7=0; else if(var7<4)var7=1; else
+													 * var7=2; this.SetFacing(var1, var2, var3, var4,
+													 * var7);//
+													 */
 		}
 	}
 }
--- src/minecraft_server/net/minecraft/src/Addon_Stained_Glass_Item.java
+++ src/minecraft_server/net/minecraft/src/Addon_Stained_Glass_Item.java
@@ -25,53 +25,66 @@ public class Addon_Stained_Glass_Item extends Item {
 	}
 
 	public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10) {
-		int damage = par1ItemStack.getItemDamage();
+		int var11 = par3World.getBlockId(par4, par5, par6);
 
-		if (par7 == 0)
-    {
-        --par5;
-    }
+		if (var11 == Block.snow.blockID && (par3World.getBlockMetadata(par4, par5, par6) & 7) < 1) {
+			par7 = 1;
+		} else if (var11 != Block.vine.blockID && var11 != Block.tallGrass.blockID && var11 != Block.deadBush.blockID) {
+			if (par7 == 0) {
+				--par5;
+			}
 
-    if (par7 == 1)
-    {
-        ++par5;
-    }
+			if (par7 == 1) {
+				++par5;
+			}
 
-    if (par7 == 2)
-    {
-        --par6;
-    }
+			if (par7 == 2) {
+				--par6;
+			}
 
-    if (par7 == 3)
-    {
-        ++par6;
-    }
+			if (par7 == 3) {
+				++par6;
+			}
 
-    if (par7 == 4)
-    {
-        --par4;
-    }
+			if (par7 == 4) {
+				--par4;
+			}
 
-    if (par7 == 5)
-    {
-        ++par4;
-    }
+			if (par7 == 5) {
+				++par4;
+			}
+		}
 
-    if (!par3World.isAirBlock(par4, par5, par6))
-    {
-        return false;
-    }
-    
-    if (!par2EntityPlayer.canPlayerEdit(par4, par5, par6, par7, par1ItemStack))
-    {
-        return false;
-    }
-    else
-    {
-    	--par1ItemStack.stackSize;
-    	par3World.setBlock(par4, par5, par6, 3003, damage, 2);
+		if (par1ItemStack.stackSize == 0) {
+			return false;
+		} else if (!par2EntityPlayer.canPlayerEdit(par4, par5, par6, par7, par1ItemStack)) {
+			return false;
+		} else if (par5 == 255 && Block.blocksList[3003].blockMaterial.isSolid()) {
+			return false;
+		} else if (par3World.canPlaceEntityOnSide(3003, par4, par5, par6, false, par7, par2EntityPlayer,
+				par1ItemStack)) {
+			Block var12 = Block.blocksList[3003];
+			int var13 = par1ItemStack.getItemDamage();
+			int var14 = Block.blocksList[3003].onBlockPlaced(par3World, par4, par5, par6, par7, par8, par9, par10,
+					var13);
 
-    	return true;
-    }
+			if (par3World.setBlock(par4, par5, par6, 3003, var14, 3)) {
+				if (par3World.getBlockId(par4, par5, par6) == 3003) {
+					Block.blocksList[3003].onBlockPlacedBy(par3World, par4, par5, par6, par2EntityPlayer, par1ItemStack);
+					Block.blocksList[3003].onPostBlockPlaced(par3World, par4, par5, par6, var14);
+					par3World.NotifyNearbyAnimalsOfPlayerBlockAddOrRemove(par2EntityPlayer, Block.blocksList[3003], par4,
+							par5, par6);
+				}
+
+				par3World.playSoundEffect((double) ((float) par4 + 0.5F), (double) ((float) par5 + 0.5F),
+						(double) ((float) par6 + 0.5F), var12.stepSound.getPlaceSound(),
+						(var12.stepSound.getVolume() + 1.0F) / 2.0F, var12.stepSound.getPitch() * 0.8F);
+				--par1ItemStack.stackSize;
+			}
+
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
