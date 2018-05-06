c2865f89bcbe2ca354bce947d1c8194ba2c3a2be
Jorge <jorgebonafe@gmail.com>
2018-05-05 22:35:58 -0300

    Fixed issues with BTTHNetServerHandler class

--- src/minecraft_server/net/minecraft/src/BTHNetServerHandler.java
+++ src/minecraft_server/net/minecraft/src/BTHNetServerHandler.java
@@ -8,38 +8,33 @@ public class BTHNetServerHandler extends NetServerHandler {
 
 	public BTHNetServerHandler(MinecraftServer aServer, INetworkManager aNetManager, EntityPlayerMP aPlayer) {
 		super(aServer, aNetManager, aPlayer);
-
+		
 		this.mcServer = aServer;
+		
+		try {
+			//if (Class.forName("net.minecraft.src.JBJorgesMiscellaneous") != null) {
+			if (Class.forName("JBJorgesMiscellaneous") != null) {
+				
+				JBJorgesMiscellaneous.sendJBMiscTest(aPlayer);
+				
+				if (aServer.getCommandManager() != null && aServer.getCommandManager() instanceof ServerCommandManager) {
+					//Object object = Class.forName("net.minecraft.src.JBCommandServerHardcoreDay").newInstance();
+					Object object = Class.forName("JBCommandServerHardcoreDay").newInstance();
+					((ServerCommandManager)aServer.getCommandManager()).registerCommand((CommandBase)object);
+				}			
+			}
+		} catch (ClassNotFoundException e) {} catch (Exception e) {}
 	}
-
+	
 	public void handleCustomPayload(Packet250CustomPayload aPacket) {
 		super.handleCustomPayload(aPacket);
 
-		try {
-			Class.forName("JBAstrolabe").getMethod("serverCustomPacketReceived", MinecraftServer.class, EntityPlayerMP.class,
-					Packet250CustomPayload.class).invoke(null, this.mcServer, this.playerEntity, aPacket);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		try {
-			Class.forName("AddonManager").getMethod("serverCustomPacketReceived", MinecraftServer.class, EntityPlayerMP.class,
-					Packet250CustomPayload.class).invoke(null, this.mcServer, this.playerEntity, aPacket);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		try {
-			Class.forName("BTHBetterThanHorses").getMethod("serverCustomPacketReceived", MinecraftServer.class,
-							EntityPlayerMP.class, Packet250CustomPayload.class)
-					.invoke(null, this.mcServer, this.playerEntity, aPacket);
-		} catch (ClassNotFoundException e) {
-		} catch (Exception e) {
-			e.printStackTrace();
+		String[] mods = new String[] { "JBJorgesMiscellaneous", "AddonManager" };
+		for (String mod : mods) {
+			try {
+				//Class.forName("net.minecraft.src."+mod).getMethod("serverCustomPacketReceived", MinecraftServer.class, EntityPlayerMP.class, Packet250CustomPayload.class).invoke(null, this.mcServer, this.playerEntity, aPacket);
+				Class.forName(mod).getMethod("serverCustomPacketReceived", MinecraftServer.class, EntityPlayerMP.class, Packet250CustomPayload.class).invoke(null, this.mcServer, this.playerEntity, aPacket);
+			} catch (ClassNotFoundException e) {} catch (Exception e) {}
 		}
 	}
 }
