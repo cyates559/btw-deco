57eae5236d2a79c60c82f104f3de4a97a1915a10
Carsen Yates <fatburgeriii@gmail.com>
2018-05-27 08:32:20 -0700

    removed old wall recipes to stay true to BTW

--- src/minecraft_server/net/minecraft/src/Addon_Walls.java
+++ src/minecraft_server/net/minecraft/src/Addon_Walls.java
@@ -7,10 +7,10 @@ public class Addon_Walls
 		FCBetterThanWolves.fcBlockStoneBrickSidingAndCorner = new AddonManager.FCBlockSidingAndCornerAndDecorative_Wall(AddonManager.ReplaceBlockID(FCBetterThanWolves.fcBlockStoneBrickSidingAndCorner),  Material.rock, "fcBlockDecorativeStoneBrick", 1.5F, 10.0F, Block.soundStoneFootstep, "fcStoneBrickSiding", "Stone Brick");
 		FCBetterThanWolves.fcBlockBrickSidingAndCorner = new AddonManager.FCBlockSidingAndCornerAndDecorative_Wall(AddonManager.ReplaceBlockID(FCBetterThanWolves.fcBlockBrickSidingAndCorner), Material.rock, "fcBlockDecorativeBrick", 2.0F, 10.0F, Block.soundStoneFootstep, "fcBrickSiding","Brick");
 
-		FCRecipes.RemoveVanillaRecipe(new ItemStack(Block.cobblestoneWall, 6, 0), new Object[] {"###", "###", '#', Block.cobblestone});
-		FCRecipes.RemoveVanillaRecipe(new ItemStack(Block.cobblestoneWall, 6, 1), new Object[] {"###", "###", '#', Block.cobblestoneMossy});
+		// FCRecipes.RemoveVanillaRecipe(new ItemStack(Block.cobblestoneWall, 6, 0), new Object[] {"###", "###", '#', Block.cobblestone});
+		// FCRecipes.RemoveVanillaRecipe(new ItemStack(Block.cobblestoneWall, 6, 1), new Object[] {"###", "###", '#', Block.cobblestoneMossy});
 
-		FCRecipes.AddVanillaRecipe(new ItemStack(Block.cobblestoneWall, 2, 0), new Object[] {"###", "###", '#', Block.cobblestone});
-		FCRecipes.AddVanillaRecipe(new ItemStack(Block.cobblestoneWall, 2, 1), new Object[] {"###", "###", '#', Block.cobblestoneMossy});
+		// FCRecipes.AddVanillaRecipe(new ItemStack(Block.cobblestoneWall, 2, 0), new Object[] {"###", "###", '#', Block.cobblestone});
+		// FCRecipes.AddVanillaRecipe(new ItemStack(Block.cobblestoneWall, 2, 1), new Object[] {"###", "###", '#', Block.cobblestoneMossy});
 	}
 }
