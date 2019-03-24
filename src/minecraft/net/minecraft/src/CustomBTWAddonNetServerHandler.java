f22dc4e9b43329317a2d4a4d00a8dcf1ca0a5d94
ammoore00 <47721416+ammoore00@users.noreply.github.com>
2019-03-23 22:00:20 -0600

    Deleted to fix cyates559 #13 (Villager trading)

--- src/minecraft/net/minecraft/src/CustomBTWAddonNetServerHandler.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.src;
-
-import net.minecraft.server.MinecraftServer;
-
-public abstract class CustomBTWAddonNetServerHandler {
-	public CustomBTWAddonNetServerHandler(MinecraftServer aServer, INetworkManager aNetManager, EntityPlayerMP aPlayer) {}
-
-	public void handleCustomPayload(Packet250CustomPayload aPacket) {}	
-}
